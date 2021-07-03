2fb0b974bde9a470ad9ed981cc26394eefedbebc
umer <umerasghar2013@gmail.com>
2021-07-04 00:17:03 +0500

    basic

--- src/components/App.js
+++ src/components/App.js
@@ -16,16 +16,47 @@ class App extends Component {
     }
   }
 
+  async loadBlockchainData() {
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+
+    const networkId = await web3.eth.net.getId();
+    const networkData = Decentragram.networks[networkId]
+
+    if (networkData) {
+      const decentragram = web3.eth.Contract(Decentragram.abi, networkData.address)
+    }
+    else {
+      window.alert('Decentragram contract not deployed to detected network.')
+    }
+  }
+
+  async loadWeb3() {
+
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    } else {
+      window.alert('Non-Etherum browser detected, You should consider trying metmask')
+    }
+  }
+
+  async componentWillMount(prevProps, prevState) {
+    await this.loadWeb3()
+    await this.loadBlockchainData();
+  }
+
   render() {
     return (
       <div>
         <Navbar account={this.state.account} />
-        { this.state.loading
+        {this.state.loading
           ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
           : <Main
-            // Code...
-            />
-          }
+          // Code...
+          />
         }
       </div>
     );
--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,5 +1,73 @@
 pragma solidity ^0.5.0;
 
 contract Decentragram {
-  // Code goes here...
+    string public name = "Decentragram";
+    uint256 public imageCount = 0;
+
+    // store Images
+    mapping(uint256 => Image) public images;
+
+    struct Image {
+        uint256 id;
+        string hash;
+        string description;
+        uint256 tipAmount;
+        address payable author;
+    }
+
+    event ImageCreated(
+        uint256 id,
+        string hash,
+        string description,
+        uint256 tipAmount,
+        address payable author
+    );
+
+    event ImageTipped(
+        uint256 id,
+        string hash,
+        string description,
+        uint256 tipAmount,
+        address payable author
+    );
+
+    // create Images
+    function uploadImage(string memory _imgHash, string memory _description)
+        public
+    {
+        require(bytes(_imgHash).length > 0); //make sure image hash exist
+        require(bytes(_description).length > 0); //make sure description exist
+        require(msg.sender != address(0x0)); //make sure uploader exist
+
+        imageCount++; //increment image id
+
+        images[imageCount] = Image(
+            imageCount,
+            _imgHash,
+            _description,
+            0,
+            msg.sender
+        );
+        emit ImageCreated(imageCount, _imgHash, _description, 0, msg.sender);
+    }
+
+    //tip image owner
+    function tipImageOwner(uint256 _id) public payable {
+        require(_id > 0 && _id <= imageCount);
+        Image memory _image = images[_id];
+
+        address payable _author = _image.author;
+        address(_author).transfer(msg.value); //pay the author by sending them Ether
+
+        _image.tipAmount += msg.value;
+
+        images[_id] = _image; //update the image
+        emit ImageTipped(
+            _id,
+            _image.hash,
+            _image.description,
+            _image.tipAmount,
+            _author
+        );
+    }
 }
